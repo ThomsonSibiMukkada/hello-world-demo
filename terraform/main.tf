f0c8b2a3cdc068815c98c23519b55f97fe009eb0
Thomson Sibi Mukkada <tsibimuk@rmplc.net>
2022-12-28 16:01:31 +0530

    teraform

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+resource "azurerm_resource_group" "aks-rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_role_assignment" "role_acrpull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  dns_prefix          = var.cluster_name
+
+  default_node_pool {
+    name                 = "systempool"
+    vm_size              = "Standard_DS2_v2"
+    orchestrator_version = var.kubernetes_version
+    availability_zones   = [1, 2, 3]
+    enable_auto_scaling  = true
+    max_count            = 3
+    min_count            = 1
+    os_disk_size_gb      = 30
+    type                 = "VirtualMachineScaleSets"
+    node_labels = {
+      "nodepool-type"    = "system"
+      "environment"      = "dev"
+      "nodepoolos"       = "linux"
+      "app"              = "system-apps" 
+    } 
+   tags = {
+      "nodepool-type"    = "system"
+      "environment"      = "dev"
+      "nodepoolos"       = "linux"
+      "app"              = "system-apps" 
+   } 
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control_enabled = true
+
+  network_profile {
+    load_balancer_sku = "Standard"
+    network_plugin    = "kubenet" 
+  }
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+resource_group_name = "aks_tf_rg"
+location            = "North Europe"
+cluster_name        = "aks-demo"
+kubernetes_version  = "1.23.12"
+system_node_count   = 1
